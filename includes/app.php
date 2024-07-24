006a33e0a8429c21ec8b0ca53839842b608b2768
mostafa93sh <mostafa.khalel.93@gmail.com>
2024-07-24 15:12:47 +0300

    adding update function

--- includes/app.php
+++ includes/app.php
@@ -1,12 +1,11 @@
 <?php
 $databaseInfo = require_once __DIR__ . '/../config/database-info.php';
-$conn = mysqli_connect('localhost', 'root', '', 'users');
+$conn = mysqli_connect('localhost', 'root', '', 'project');
 if (!$conn) {
     echo 'Something went wrong';
 } else {
     echo 'connected to database successfully';
+    echo '<br/>';
 }
 
 require_once 'helper.php';
-
-mysqli_close($conn);
--- includes/helper.php
+++ includes/helper.php
@@ -1,4 +1,11 @@
 <?php
+
+/**
+ * Insert function
+ * @param string $table
+ * @param array $data
+ * @return array as assoc
+ */
 if (!function_exists('db_create')) {
     function db_create(string $table, array $data)
     {
@@ -24,4 +31,39 @@ if (!function_exists('db_create')) {
     }
 }
 
-var_dump(db_create('users', ['name' => 'mostafa', 'email' => 'mostafa@test.com']));
+// var_dump(db_create('users', ['name' => 'ahmed', 'email' => 'ahmed@test.com']));
+// var_dump(db_create('users', ['name' => 'mostafa', 'email' => 'mostafa@test.com']));
+/**
+ * Update function
+ * @param string $table 
+ * @param array $data
+ * @param int $id
+ * @return array as assoc
+ */
+
+if (!function_exists('db_update')) {
+    function db_update(string $table, array $data, int $id)
+    {
+        global $conn;
+        $sql = "UPDATE $table set ";
+        $set = "";
+        // $values = "";
+        foreach ($data as $key => $value) {
+            $set .= "$key = '$value',";
+            // $values .= " '" . $value . "',";
+        }
+
+        $set = rtrim($set, ",");
+        // $values = rtrim($values, ",");
+
+        $sql .= "$set where id = $id";
+        // echo $sql;
+        mysqli_query($conn, $sql);
+
+        $first = mysqli_query($conn, "select * from $table where id = $id");
+        mysqli_close($conn);
+        return mysqli_fetch_assoc($first);
+    }
+}
+
+var_dump(db_update('users', ['name' => 'ahmed2', 'email' => 'ahmed2@test.com'], 1));
